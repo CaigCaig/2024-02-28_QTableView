6b02b7cdcb573b24ee3941e9e7e92f80a3853506
CaigCaig <caig@inbox.ru>
2024-03-05 16:03:45 +0300

    Initial commit

---
+++ database.cpp
@@ -0,0 +1,310 @@
+#include "database.h"
+
+DataBase::DataBase(QObject *parent)
+    : QObject{parent}
+{
+
+    dataBase = new QSqlDatabase();
+
+    simpleQuery = new QSqlQuery();
+
+    tableWidget = new QTableWidget();
+
+    tableModel = new QSqlTableModel();
+    tableView = new QTableView();
+    //tableView->setModel(tableModel);
+
+    queryModel = new QSqlQueryModel();
+    headers = new QStringList();
+
+}
+
+DataBase::~DataBase()
+{
+    delete dataBase;
+    //delete simpleQuery;
+    delete queryModel;
+    delete tableView;
+    delete headers;
+}
+
+/*!
+ * \brief Метод добавляет БД к экземпляру класса QSqlDataBase
+ * \param driver драйвер БД
+ * \param nameDB имя БД (Если отсутствует Qt задает имя по умолчанию)
+ */
+void DataBase::AddDataBase(QString driver, QString nameDB)
+{
+
+    *dataBase = QSqlDatabase::addDatabase(driver, nameDB);
+
+}
+
+/*!
+ * \brief Метод подключается к БД
+ * \param для удобства передаем контейнер с данными необходимыми для подключения
+ * \return возвращает тип ошибки
+ */
+void DataBase::ConnectToDataBase(QVector<QString> data)
+{
+
+    dataBase->setHostName(data[hostName]);
+    dataBase->setDatabaseName(data[dbName]);
+    dataBase->setUserName(data[login]);
+    dataBase->setPassword(data[pass]);
+    dataBase->setPort(data[port].toInt());
+
+
+    ///Тут должен быть код ДЗ
+
+
+    bool status;
+    status = dataBase->open( );
+    emit sig_SendStatusConnection(status);
+
+}
+/*!
+ * \brief Метод производит отключение от БД
+ * \param Имя БД
+ */
+void DataBase::DisconnectFromDataBase(QString nameDb)
+{
+
+    *dataBase = QSqlDatabase::database(nameDb);
+    dataBase->close();
+
+}
+/*!
+ * \brief Метод формирует запрос к БД.
+ * \param request - SQL запрос
+ * \return
+ */
+void DataBase::RequestToDB(QString request)
+{
+
+    ///Тут должен быть код ДЗ
+    //*simpleQuery = QSqlQuery(*dataBase);
+    queryModel->setQuery(request);
+    for (uint32_t i = 0, j = 0; i < queryModel->columnCount(); i++, j++)
+    {
+        queryModel->setHeaderData(i, Qt::Horizontal, headers[j]);
+    }
+    QSqlError err;
+    emit sig_SendStatusRequest(err, headers);
+
+    /*
+    if (simpleQuery->exec(request) == false)
+    {
+        err = simpleQuery->lastError();
+    }
+    */
+    //emit sig_SendStatusRequest(err);
+
+}
+
+
+void DataBase::ReadAnswerFromDB(int requestType)
+{
+    /*
+     * Используем оператор switch для разделения запросов
+    */
+    QStringList hdrs;
+    //QString str;
+    QVariantList vlist;
+    uint32_t conterRows = 0;
+
+    switch (requestType) {
+    //Для наших запросов вид таблицы не поменяетя. Поэтому будет единый обработчик.
+    case requestAllFilms:
+        //tableModel-
+        tableModel->setHeaderData(0, Qt::Horizontal, "Название");
+        tableModel->setHeaderData(1, Qt::Horizontal, "Год выпуска");
+        tableModel->setHeaderData(2, Qt::Horizontal, "Жанр");
+
+        //queryModel->
+
+
+        /*
+        tableWidget->setColumnCount(3);
+        tableWidget->setRowCount(0);
+        hdrs << "Название" << "Год выпуска" << "Жанр";
+        tableWidget->setHorizontalHeaderLabels(hdrs);
+        while(simpleQuery->next()){
+            tableWidget->insertRow(conterRows);
+            for(int i = 0; i<tableWidget->columnCount(); ++i){
+
+                str = simpleQuery->value(i).toString();
+                QTableWidgetItem *item = new QTableWidgetItem(str);
+                tableWidget->setItem(tableWidget->rowCount() - 1, i, item);
+
+            }
+            ++conterRows;
+        }
+        */
+        break;
+    case requestComedy:
+        /*
+        tableWidget->setColumnCount(2);
+        tableWidget->setRowCount(0);
+        hdrs << "Название" << "Описание";
+        tableWidget->setHorizontalHeaderLabels(hdrs);
+        while(simpleQuery->next()){
+            tableWidget->insertRow(conterRows);
+            for(int i = 0; i<tableWidget->columnCount(); ++i){
+
+                str = simpleQuery->value(i).toString();
+                QTableWidgetItem *item = new QTableWidgetItem(str);
+                tableWidget->setItem(tableWidget->rowCount() - 1, i, item);
+
+            }
+            ++conterRows;
+        }
+        */
+        break;
+    case requestHorrors:
+    {
+        /*
+         * Объект tableWidget представляет из себя таблицу, которую можно отобразить
+         * в ПИ. Зfдадим ее параметры и заголовки. Мы получаем из БД название фильма, год выпуска и жанр
+        */
+
+        /* QSqlQuery
+         * После запроса к БД ответ помещается в объект QSqlQuery.
+         * Его необходимо считвать построчно. Для чтения ответа используется метод
+         * next, он работает следующим образом: при вызове метода указатель перемещается
+         * на следующую запись, в ответе. После получения ответа он указывает, если можно так
+         * выразится на -1 запись. Т.е. при первом вызове метода next указатель переместится
+         * на первую запись, а сам метод вернет true. В случае если указатель переместится
+         * за диапазон записей метод вернет false. Также он вернет false в случае отсутствия
+         * записей.
+         * Каждая запись является стройкой прочитанной из БД. Для перемещения по столбцам
+         * используется метод QSqlQuery value(индекс столбца).
+        */
+        /* QTableWidget
+         * Необходимо сформировать таблицу, которую потом передадим в MainWindow для отображения
+         * Для каждой записи прочитанной при помощи метода next будем добавлять строку в tableWidget
+         * и заполнять значениями из ответа. Каждая ячейка таблицы является объектом класса
+         * QTableWidgetItem. Т.е. для заполнения каждой ячеки мы должны создать экземпляр этого метода.
+         * Значение ячейки в таблицк устанавливается при помощи метода setItem.
+        */
+        /*
+         * После заполнения всей таблицы мы ее передаем в MainWindow при помощи сигнала.
+        */
+
+        /*
+        tableWidget->setColumnCount(2);
+        tableWidget->setRowCount(0);
+        hdrs << "Название" << "Описание";
+        tableWidget->setHorizontalHeaderLabels(hdrs);
+        while(simpleQuery->next()){
+            tableWidget->insertRow(conterRows);
+
+            for(int i = 0; i<tableWidget->columnCount(); ++i){
+
+                str = simpleQuery->value(i).toString();
+                QTableWidgetItem *item = new QTableWidgetItem(str);
+                tableWidget->setItem(tableWidget->rowCount() - 1, i, item);
+
+            }
+            ++conterRows;
+        }
+        */
+
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    tableView->setModel(tableModel);
+    tableModel->select();
+
+    emit sig_SendDataFromDB(tableView, requestType);
+
+}
+
+/*
+void DataBase::ReadAnswerFromDB(int requestType)
+{
+    QStringList hdrs;
+    QString str;
+    uint32_t conterRows = 0;
+
+    switch (requestType) {
+    //Для наших запросов вид таблицы не поменяетя. Поэтому будет единый обработчик.
+    case requestAllFilms:
+        tableWidget->setColumnCount(3);
+        tableWidget->setRowCount(0);
+        hdrs << "Название" << "Год выпуска" << "Жанр";
+        tableWidget->setHorizontalHeaderLabels(hdrs);
+        while(simpleQuery->next()){
+            tableWidget->insertRow(conterRows);
+            for(int i = 0; i<tableWidget->columnCount(); ++i){
+
+                str = simpleQuery->value(i).toString();
+                QTableWidgetItem *item = new QTableWidgetItem(str);
+                tableWidget->setItem(tableWidget->rowCount() - 1, i, item);
+
+            }
+            ++conterRows;
+        }
+        break;
+    case requestComedy:
+        tableView->setColumn
+        tableWidget->setColumnCount(2);
+        tableWidget->setRowCount(0);
+        hdrs << "Название" << "Описание";
+        tableWidget->setHorizontalHeaderLabels(hdrs);
+        while(simpleQuery->next()){
+            tableWidget->insertRow(conterRows);
+            for(int i = 0; i<tableWidget->columnCount(); ++i){
+
+                str = simpleQuery->value(i).toString();
+                QTableWidgetItem *item = new QTableWidgetItem(str);
+                tableWidget->setItem(tableWidget->rowCount() - 1, i, item);
+
+            }
+            ++conterRows;
+        }
+        break;
+    case requestHorrors:
+    {
+        tableWidget->setColumnCount(2);
+        tableWidget->setRowCount(0);
+        hdrs << "Название" << "Описание";
+        tableWidget->setHorizontalHeaderLabels(hdrs);
+        while(simpleQuery->next()){
+            tableWidget->insertRow(conterRows);
+
+            for(int i = 0; i<tableWidget->columnCount(); ++i){
+
+                str = simpleQuery->value(i).toString();
+                QTableWidgetItem *item = new QTableWidgetItem(str);
+                tableWidget->setItem(tableWidget->rowCount() - 1, i, item);
+
+            }
+            ++conterRows;
+        }
+
+        //emit sig_SendDataFromDB(tableWidget, requestType);
+
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    emit sig_SendDataFromDB(tableWidget, requestType);
+
+}
+*/
+
+/*!
+ * @brief Метод возвращает последнюю ошибку БД
+ */
+QSqlError DataBase::GetLastError()
+{
+    return dataBase->lastError();
+}
---
+++ database.h
@@ -0,0 +1,75 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QTableWidget>
+#include <QTableView>
+#include <QObject>
+#include <QSqlError>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+#include <QSqlQueryModel>
+
+#define POSTGRE_DRIVER "QPSQL"
+#define DB_NAME "MyDB"
+
+//Количество полей данных необходимых для подключения к БД
+#define NUM_DATA_FOR_CONNECT_TO_DB 5
+
+//Перечисление полей данных
+enum fieldsForConnect{
+    hostName = 0,
+    dbName = 1,
+    login = 2,
+    pass = 3,
+    port = 4
+};
+
+//Типы запросов
+enum requestType{
+
+    requestAllFilms = 1,
+    requestComedy   = 2,
+    requestHorrors  = 3
+
+};
+
+
+
+class DataBase : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit DataBase(QObject *parent = nullptr);
+    ~DataBase();
+
+    void AddDataBase(QString driver, QString nameDB = "");
+    void DisconnectFromDataBase(QString nameDb = "");
+    void RequestToDB(QString request);
+    void ReadAnswerFromDB(int answerType);
+    QSqlError GetLastError(void);
+    void ConnectToDataBase(QVector<QString> dataForConnect);
+
+
+signals:
+
+    //void sig_SendDataFromDB(const QTableWidget *tableWg, int typeR);
+    void sig_SendDataFromDB(const QTableView *tableVw, int typeR);
+    void sig_SendStatusConnection(bool);
+    //void sig_SendStatusRequest(QSqlError err);
+    void sig_SendStatusRequest(QSqlError err, const QStringList *headers);
+
+private:
+
+    QSqlDatabase* dataBase;
+    QSqlQuery* simpleQuery;
+    QTableWidget* tableWidget;
+    QTableView* tableView;
+    QSqlQueryModel* queryModel;
+    QSqlTableModel* tableModel;
+    QStringList* headers;
+
+};
+
+#endif // DATABASE_H
